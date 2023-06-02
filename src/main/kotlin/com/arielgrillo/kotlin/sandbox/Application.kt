043e80d76021a8a8d06189809d6ef5785d2ccbbe
Ariel Grillo <ariel.grillo@adevinta.com>
2023-06-02 09:23:39 +0200

    Trying to fix dialect

--- src/main/kotlin/com/arielgrillo/kotlin/sandbox/Application.kt
+++ src/main/kotlin/com/arielgrillo/kotlin/sandbox/Application.kt
@@ -1,15 +1,9 @@
 package com.arielgrillo.kotlin.sandbox
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EntityScan(
-	basePackages = [
-		"com.arielgrillo.kotlin.sandbox.infrastructure",
-	]
-)
 class Application
 
 fun main(args: Array<String>) {
--- src/test/kotlin/com/arielgrillo/kotlin/sandbox/ApplicationTests.kt
+++ src/test/kotlin/com/arielgrillo/kotlin/sandbox/ApplicationTests.kt
@@ -1,9 +1,16 @@
 package com.arielgrillo.kotlin.sandbox
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@EntityScan(
+	basePackages = [
+		"com.arielgrillo.kotlin.sandbox.infrastructure",
+	]
+)
+
 class ApplicationTests {
 
 	@Test
